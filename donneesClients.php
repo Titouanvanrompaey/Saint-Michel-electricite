a5231105b78c4f77b4206bad8e03d7acf11f5320
Titouan van rompaey <2017tvr@gmail.com>
2024-01-22 21:43:23 +0100

    avancement dans la base de données

---
+++ donneesClients.php
@@ -0,0 +1,11 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "infosclients";
+
+    $nom = $_POST["nom"];
+    $email = $_POST["email"];
+    $objet = $_POST["objet"];
+    $message = $_POST["message"];
+?>
