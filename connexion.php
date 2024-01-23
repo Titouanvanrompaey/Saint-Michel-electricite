ee99c0d50c8aae70dea8f1d9a89b3b40f60deb3d
Titouan van rompaey <2017tvr@gmail.com>
2024-01-23 18:34:46 +0100

    connexion base de données et formulaire fonctionnels

--- connexion.php
+++ connexion.php
@@ -1,14 +1,16 @@
 <?php
 $server = "localhost";
+$dbname = "infosclients";
 $user = "root";
 $password = "";
-$dbname = "infosclients";
-// Create connection
-$conn = new mysqli($server, $user, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+
+try {
+    // Créer une connexion PDO
+    $conn = new PDO("mysql:host=$server;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connecté à la base de données";
+    $conn->exec("SET NAMES 'utf8'");
+} catch (PDOException $e) {
+    die("Impossible de se connecter à la base de données. Erreur : " . $e->getMessage());
 }
-echo "connecté";
-$conn->set_charset("utf8");
 ?>
--- donneesClients.php
+++ donneesClients.php
@@ -1,11 +1,22 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $dbname = "infosclients";
+require('connexion.php');
 
-    $nom = $_POST["nom"];
-    $email = $_POST["email"];
-    $objet = $_POST["objet"];
-    $message = $_POST["message"];
+$nom = $_POST["nom"];
+$email = $_POST["email"];
+$objet = $_POST["objet"];
+$message = $_POST["message"];
+
+try {
+    // Utilisez la connexion PDO stockée dans la variable $conn
+    $stmt = $conn->prepare("INSERT INTO `client` (nom, email, objet, message) VALUES (:nom, :email, :objet, :message)");
+    $stmt->bindParam(':nom', $nom);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':objet', $objet);
+    $stmt->bindParam(':message', $message);
+  
+    $stmt->execute();
+    echo "Données insérées avec succès!";
+} catch (PDOException $e) {
+    echo 'Impossible de traiter les données. Erreur : ' . $e->getMessage();
+}
 ?>
